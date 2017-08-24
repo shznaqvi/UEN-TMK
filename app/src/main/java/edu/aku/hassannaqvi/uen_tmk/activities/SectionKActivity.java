90579c590d2e3d152d47f7ab15e2f9285422c3b6
ali.azaz <openmind_ali@hotmail.com>
2017-08-24 17:40:56 +0500

    layout designed of activities E,I and K

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk/activities/SectionKActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.uen_tmk.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SectionKActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_k);
+    }
+}
