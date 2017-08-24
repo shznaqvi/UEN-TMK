ec793b1274f51a4a19d1c8af47ccb0a3cdf82bba
gul.sanober <gulsanobermemon@gmail.com>
2017-08-24 16:58:56 +0500

    Section H, J, FA Layouts

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk/activities/SectionJActivity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.uen_tmk.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.uen_tmk.R;
+
+public class SectionJActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_j);
+    }
+}
