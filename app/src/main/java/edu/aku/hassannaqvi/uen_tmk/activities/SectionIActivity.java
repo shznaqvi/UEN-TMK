7b6c4bf085a8fa8607228d68d86e10afa99a8ad7
javed.khan <javedak09@gmail.com>
2017-09-08 11:06:48 +0500

    Crash Remove Section I

--- app/src/main/java/edu/aku/hassannaqvi/uen_tmk/activities/SectionIActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk/activities/SectionIActivity.java
@@ -586,8 +586,8 @@ public class SectionIActivity extends AppCompatActivity {
     @BindView(R.id.fldGrpti03)
     LinearLayout fldGrpti03;
 
-    @BindViews({R.id.bcgDate, R.id.opv0Date, R.id.opv1Date, R.id.penta1Date, R.id.pcv1Date, R.id.opv2Date, R.id.pcv2Date,
-            R.id.opv3Date, R.id.penta3Date, R.id.pcv3Date, R.id.ipvDate, R.id.measles1Date, R.id.measles2Date})
+    @BindViews({R.id.bcgDate, R.id.opv0Date, R.id.opv1Date, R.id.penta1Date, R.id.pcv1Date, R.id.opv2Date, R.id.penta2Date
+            , R.id.pcv2Date, R.id.opv3Date, R.id.penta3Date, R.id.pcv3Date, R.id.ipvDate, R.id.measles1Date, R.id.measles2Date})
     List<DatePickerInputEditText> dates;
 
     @BindViews({R.id.fldGrpBcgC, R.id.fldGrpopv0C, R.id.fldGrpPenta1C, R.id.fldGrppcv1C, R.id.fldGrpopv1C, R.id.fldGrppenta2C,
