22002fdc31fe3fdd8add79d7171b5b4a1c035599
javed.khan <javedak09@gmail.com>
2017-09-22 17:10:15 +0500

    Section B New Quest added

--- app/src/main/java/edu/aku/hassannaqvi/uen_tmk/activities/SectionBActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk/activities/SectionBActivity.java
@@ -156,6 +156,17 @@ public class SectionBActivity extends AppCompatActivity {
     @BindView(R.id.btn_ContNextSec)
     Button btn_ContNextSec;
 
+    @BindView(R.id.fldGrptb11)
+    LinearLayout fldGrptb11;
+
+    @BindView(R.id.tb12)
+    RadioGroup tb12;
+    @BindView(R.id.tb12a)
+    RadioButton tb12a;
+    @BindView(R.id.tb12b)
+    RadioButton tb12b;
+
+
     DatabaseHelper db;
     long ageInyears = 0;
 
@@ -233,7 +244,19 @@ public class SectionBActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
                 if (i == R.id.tb04a) {
                     tb10a.setEnabled(false);
+
+                    if (tb11b.isChecked()) {
+                        fldGrptb11.setVisibility(View.VISIBLE);
+                    } else {
+                        tb12.clearCheck();
+                        fldGrptb11.setVisibility(View.GONE);
+                    }
+
                 } else {
+
+                    tb12.clearCheck();
+                    fldGrptb11.setVisibility(View.GONE);
+
                     tb10a.setEnabled(true);
                 }
             }
@@ -395,6 +418,25 @@ public class SectionBActivity extends AppCompatActivity {
             }
         });
 
+        tb11.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                if (tb11b.isChecked()) {
+
+                    if (tb04b.isChecked()) {
+                        fldGrptb11.setVisibility(View.VISIBLE);
+                    } else {
+                        tb12.clearCheck();
+                        fldGrptb11.setVisibility(View.GONE);
+                    }
+
+                } else {
+                    tb12.clearCheck();
+                    fldGrptb11.setVisibility(View.GONE);
+                }
+            }
+        });
+
 
     }
 
@@ -499,6 +541,8 @@ public class SectionBActivity extends AppCompatActivity {
                 : "0");
         sB.put("tb11", tb11a.isChecked() ? "1" : tb11b.isChecked() ? "2"
                 : tb11c.isChecked() ? "3" : tb11d.isChecked() ? "4" : tb11e.isChecked() ? "5" : "0");
+
+        sB.put("tb12", tb12a.isChecked() ? "1" : tb12b.isChecked() ? "2" : "0");
 
 
         MainApp.ageRdo = tbdob.indexOfChild(findViewById(tbdob.getCheckedRadioButtonId())) + 1;
@@ -726,6 +770,18 @@ public class SectionBActivity extends AppCompatActivity {
                 tb09.setError(null);
             }
 
+        }
+
+        if (tb04b.isChecked() && tb11b.isChecked()) {
+            if (tb12.getCheckedRadioButtonId() == -1) {
+                Toast.makeText(this, "ERROR(empty): " + getString(R.string.tiRespondentMother), Toast.LENGTH_SHORT).show();
+                tb12a.setError("This data is Required!");    // Set Error on last radio button
+
+                Log.i(TAG, "tb12a: This data is Required!");
+                return false;
+            } else {
+                tb12a.setError(null);
+            }
         }
 
 
